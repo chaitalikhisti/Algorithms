5ebf42c56e89d35645b3d82ee0650f75e87c7158
Chaitali Khisti <chaitalikhisti@users.noreply.github.com>
2017-08-29 22:31:21 -0700

    BubbleSort C-sharp

---
+++ src/BubbleSort/BubbleSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortingAlgorithm
+{
+    class Program
+    {
+        static void BubbleSort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < (n - 1) - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(array,  j, j + 1);
+                    }
+                }
+            }            
+        }
+
+        static void Swap(int[] someArray, int bigNo, int smallNo)
+        {
+            int temp = someArray[bigNo];
+            someArray[bigNo] = someArray[smallNo];
+            someArray[smallNo] = temp;
+        }
+
+        static void DisplayArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bubble Sort");
+            Console.Write("Number of elements in array: ");
+            int arrayLength = Int32.Parse(Console.ReadLine());
+            int[] arrayToBeSorted = new int[arrayLength];
+            for (int i = 0; i < arrayToBeSorted.Length; i++)
+            {
+                arrayToBeSorted[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.Write("Unsorted Array: ");
+            DisplayArray(arrayToBeSorted);
+            BubbleSort(arrayToBeSorted);
+            Console.Write("Sorted Array: ");
+            DisplayArray(arrayToBeSorted);
+        }
+    }
+}
